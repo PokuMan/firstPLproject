7261a0ee04f35d257c5b7bba0744df3a2d851088
PokuMan <game.andlua@gmail.com>
2023-06-27 10:51:39 +0800

    test

--- src/main/java/me/pokumen/first/First.java
+++ src/main/java/me/pokumen/first/First.java
@@ -7,13 +7,10 @@ public final class First extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        System.out.println("Hello World");
-
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        System.out.println("Done");
     }
 }
