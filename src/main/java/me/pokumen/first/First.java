7eee9ca344651d56d8ad7980fa96aa220da6b643
PokuMan <game.andlua@gmail.com>
2023-06-29 20:34:50 +0800

    test 1

--- src/main/java/me/pokumen/first/First.java
+++ src/main/java/me/pokumen/first/First.java
@@ -1,16 +1,36 @@
 package me.pokumen.first;
 
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.Command;
+import org.bukkit.entity.Player;
 
 public final class First extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getLogger().info("Plugin Di aktfkan");
+
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("Plugin Di non aktfkan");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+     if (cmd.getName().equalsIgnoreCase("hello")) {
+         if (sender instanceof Player) {
+             Player player = (Player) sender;
+             player.sendMessage("Hallo, Selamat Datang Di Server");
+         } else {
+             sender.sendMessage("Perintah ini hanya dapat digunakan oleh pemain.");
+         }
+         return true;
+     }
+     return true;
     }
 }
